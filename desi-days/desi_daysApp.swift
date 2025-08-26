c805abe627cbc31b12cef8151caa2ee2e1cc6e91
Pallavi Badanahatti <pallavibadan@gmail.com>
2025-08-26 18:02:07 -0400

    Initial Commit

---
+++ desi-days/desi_daysApp.swift
@@ -0,0 +1,17 @@
+//
+//  desi_daysApp.swift
+//  desi-days
+//
+//  Created by Pallavi Badanahatti on 8/26/25.
+//
+
+import SwiftUI
+
+@main
+struct desi_daysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ desi-daysTests/desi_daysTests.swift
@@ -0,0 +1,16 @@
+//
+//  desi_daysTests.swift
+//  desi-daysTests
+//
+//  Created by Pallavi Badanahatti on 8/26/25.
+//
+
+import Testing
+
+struct desi_daysTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
